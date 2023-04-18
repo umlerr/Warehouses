cf27ae2a55ded69a2b2ff92a875115ecd83d4461
umlerr <ugrumov.misha@yandex.ru>
2023-04-18 13:50:14 +0300

    // test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,11 @@
-module sample {
+module oop.Interface {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.xml;
     requires java.desktop;
     requires java.sql;
-    requires jdk.scripting.nashorn;
     requires itextpdf;
 
-
-    opens oop.Interface to javafx.fxml;
-    exports oop.Interface;
+    opens warehouses.Interface to javafx.fxml;
+    exports warehouses.Interface;
 }
